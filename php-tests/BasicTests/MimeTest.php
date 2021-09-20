345446eec954a67d86f5abc781f662ab313b3a4c
Petr Plsek <me@kalanys.com>
2021-09-20 02:31:06 +0200

    kw_mime - added extensions, add test of that dynamical part, add testfiles

--- php-tests/BasicTests/MimeTest.php
+++ php-tests/BasicTests/MimeTest.php
@@ -11,9 +11,30 @@ class MimeTest extends CommonTestClass
 {
     public function testBasic(): void
     {
-        new MimeType(true);
         $this->assertEquals('video/webm', MimeType::mimeByExt('webm'));
         $this->assertEquals('text/pascal', MimeType::mimeByExt('PAS'));
         $this->assertEquals('application/octet-stream', MimeType::mimeByExt('FOO'));
+    }
+
+    /**
+     * @param string $file
+     * @param string $mime
+     * @param bool $localAtFirst
+     * @dataProvider fullProvider
+     */
+    public function testFull(string $file, string $mime, bool $localAtFirst): void
+    {
+        $lib = new MimeType($localAtFirst);
+        $this->assertEquals($mime, $lib->mimeByPath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . $file));
+    }
+
+    public function fullProvider(): array
+    {
+        return [
+            ['test.class', 'text/x-c', false],
+            ['test.pas', 'text/plain', false],
+            ['test.class', 'application/java-vm', true],
+            ['test.pas', 'text/pascal', true],
+        ];
     }
 }
---
+++ php-tests/data/test.pas
@@ -0,0 +1,6 @@
+program mime_example;
+
+begin
+  {dummy Pascal program for testing file mime}
+  writeln('Pascal here!');
+end.
