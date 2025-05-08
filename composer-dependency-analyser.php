bdf96de16058e280d33b5fb4149ee0416939dd2e
Petr Plsek <me@kalanys.com>
2025-05-08 22:56:47 +0200

    kw_mime - dependency on kw_mime - not every time it's necessary 3

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -12,12 +12,6 @@ $config = new Configuration();
 
 return $config
     // ignore errors on specific packages and paths
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/php-src/Check/Traits/TStorage.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Check/DataFiles.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/php-src/Check/DataStorage.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Check/ResourceFiles.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Check/TempFiles.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/php-src/Check/Factory.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Check/Factory.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPath(__DIR__ . '/php-src/Check/LocalVolume2.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    // ext-mime for some, other kw_* packages on others
+    ->ignoreErrors([ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
